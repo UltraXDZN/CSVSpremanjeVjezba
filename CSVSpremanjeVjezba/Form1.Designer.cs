49b73625612bd88e8fc4a969ec37a933c5b82bb9
UltraXDZN <ultraxdzn@gmail.com>
2021-04-12 08:49:50 +0200

    Init commit

---
+++ CSVSpremanjeVjezba/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace CSVSpremanjeVjezba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.godRodTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(102, 9);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.imeTextBox.TabIndex = 1;
+            // 
+            // prezimeTextBox
+            // 
+            this.prezimeTextBox.Location = new System.Drawing.Point(102, 35);
+            this.prezimeTextBox.Name = "prezimeTextBox";
+            this.prezimeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.prezimeTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prezime:";
+            // 
+            // godRodTextBox
+            // 
+            this.godRodTextBox.Location = new System.Drawing.Point(102, 61);
+            this.godRodTextBox.Name = "godRodTextBox";
+            this.godRodTextBox.Size = new System.Drawing.Size(100, 20);
+            this.godRodTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Godina rođenja:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(102, 87);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "E-mail:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(21, 135);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(127, 135);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 9;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 180);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.godRodTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.prezimeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.imeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.TextBox prezimeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox godRodTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button LoadButton;
+    }
+}
+
---
+++ CSVSpremanjeVjezba/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVSpremanjeVjezba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string lokacija = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments), "testNiOP");
+
+            string saved_data = imeTextBox.Text + "," + prezimeTextBox.Text + "," + godRodTextBox.Text + "," + emailTextBox.Text + ",";
+
+            if (!File.Exists(lokacija))
+            {
+                Directory.CreateDirectory(lokacija);
+            }
+            StreamWriter sw = new StreamWriter(lokacija + "/PrezimeImeSystemNiOP.csv");
+            sw.Write(saved_data);
+            sw.Close();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            string lokacija = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments), "testNiOP");
+
+            if (!File.Exists(lokacija))
+            {
+                Directory.CreateDirectory(lokacija);
+            }
+            StreamReader sw = new StreamReader(lokacija + "/PrezimeImeSystemNiOP.csv");
+            string saved_data = sw.ReadToEnd();
+            string[] podaci = saved_data.Split(',');
+            imeTextBox.Text = podaci[0];
+            prezimeTextBox.Text = podaci[1];
+            godRodTextBox.Text = podaci[2];
+            emailTextBox.Text = podaci[3];
+            sw.Close();
+        }
+    }
+}
